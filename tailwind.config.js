608a8f485e2b0d5826a0a865b965dabcf0ccb89a
Tozo sensei <codersensei@outlook.com>
2024-12-30 20:17:24 -0800

    fix(style): add some custom space and attributes

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,18 @@ export default {
     "./src/**/*.{html,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      gridTemplateColumns: {
+        custom: 'repeat(3, minmax(200px, 1fr))', // Custom grid template
+        custom1: 'repeeat(auto-fill, minmax(200px, 1fr))', // Custom grid template
+      },
+      fontFamily: {
+        'inter': ['Inter', 'sans-serif', 'Roboto'], // Add Inter font
+      },
+      boxShadow: {
+        'custom': '0px 0px 0px 1px rgba(0, 0, 0, 0.06), 0px 5px 22px rgba(0, 0, 0, 0.04)', // Custom box shadow
+      },
+    },
   },
   plugins: [],
 }
