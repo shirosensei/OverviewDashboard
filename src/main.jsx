25c9e1ae034a79a864bc3208731b7c8b903ff728
Tozo sensei <codersensei@outlook.com>
2024-12-30 20:25:34 -0800

    feat(main.jsx): add the font paths relative to the main compoent

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './assets/css/index.css';
+import './assets/css/fonts.css';
+
 
 import App from './App.jsx'
 
